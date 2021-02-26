a1571144c336390d085d8e3602bc73542f1c5ba9
Pierre-Yves.AUGEM <pierreyves.augem@gmail.com>
2021-02-26 16:16:17 +0100

    modif script sql + suppr classe inutile

--- cryptomonnaie/src/main/java/com/cda/jee/Currency.java
+++
@@ -1,5 +0,0 @@
-package com.cda.jee;
-
-public class Currency {
-
-}
--- cryptomonnaie/src/main/resources/script_ddl_crypto.sql
+++ cryptomonnaie/src/main/resources/script_ddl_crypto.sql
@@ -3,17 +3,18 @@ GRANT ALL PRIVILEGES ON cda_cryptomonnaie.* TO 'cda1'@'%';
 
 CREATE TABLE IF NOT EXISTS currency(
    id_currency INT AUTO_INCREMENT NOT NULL,
-   label_currency VARCHAR(50) NOT NULL,
+   name_currency VARCHAR(50) NOT NULL,
+   label_currency VARCHAR(3) NOT NULL,
    current_price DECIMAL(15,2) NOT NULL,
-   PRIMARY KEY(id)
+   PRIMARY KEY(id_currency)
 );
 
-CREATE TABLE IF NOT EXISTS avoirs(
-   id_avoirs INT AUTO_INCREMENT NOT NULL,
-   date_achat DATE NOT NULL,
-   prix_achat_unite DECIMAL(15,2) NOT NULL,
-   nbre_unite INT NOT NULL,
+CREATE TABLE IF NOT EXISTS holding(
+   id_holding INT AUTO_INCREMENT NOT NULL,
+   purchase_date DATE NOT NULL,
+   unit_purchase_price DECIMAL(15,2) NOT NULL,
+   quantity INT NOT NULL,
    id_currency INT NOT NULL,
-   PRIMARY KEY(id_avoirs),
+   PRIMARY KEY(id_holding),
    FOREIGN KEY(id_currency) REFERENCES currency(id_currency)
 );
