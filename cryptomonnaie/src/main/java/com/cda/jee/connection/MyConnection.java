e8af82f6ff624d0a13d377873d62e7456b7c4ec0
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-02-26 18:06:52 +0100

    Set the connection to the database

--- cryptomonnaie/src/main/java/com/cda/jee/connection/MyConnection.java
+++ cryptomonnaie/src/main/java/com/cda/jee/connection/MyConnection.java
@@ -1,5 +1,44 @@
 package com.cda.jee.connection;
 
-public class MyConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+
+public class MyConnection implements AutoCloseable {
+	private static Connection connection = null;
+
+	private MyConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Properties appProps = new Properties();
+			appProps.load(MyConnection.class.getResourceAsStream("/db.properties"));
+			connection = DriverManager.getConnection(
+					appProps.getProperty("url"), 
+					appProps.getProperty("user"),
+					appProps.getProperty("password"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() {
+		if (connection == null) {
+			new MyConnection();
+		}
+		return connection;
+	}
+
+	@Override
+	public void close() throws Exception {
+		if (connection != null) {
+			try {
+				connection.close();
+				System.out.println("Thanks!");
+			} catch (SQLException ignore) {
+				ignore.printStackTrace();
+			}
+		}
+	}
 }
