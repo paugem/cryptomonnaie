3143e3b9bb11011765b71d1b2af5478dd8567ced
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-02-27 22:08:04 +0100

    set the show currency servlet + jsp

--- cryptomonnaie/src/main/java/com/cda/jee/controller/CurrencyShowServlet.java
+++ cryptomonnaie/src/main/java/com/cda/jee/controller/CurrencyShowServlet.java
@@ -1,6 +1,7 @@
 package com.cda.jee.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +9,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.cda.jee.model.Currency;
+import com.cda.jee.services.CurrencyServicesImp;
+
 @WebServlet("/currency_show.html")
 public class CurrencyShowServlet extends HttpServlet {
+	CurrencyServicesImp currencyServices = new CurrencyServicesImp();
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String idStr = req.getParameter("id");
+		int id = Integer.valueOf(idStr);
+		Currency currency = currencyServices.read(id);
+
+		req.setAttribute("currency", currency);
 
 		req.getRequestDispatcher("/WEB-INF/currency_show.jsp").forward(req, resp);
 	}
+	
 }
