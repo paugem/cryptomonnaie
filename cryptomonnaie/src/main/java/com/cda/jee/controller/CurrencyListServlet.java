baf8099982517012147dc26bd6dc986b0b27c679
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-02-27 16:30:56 +0100

    set the index currency servlet + jsp

--- cryptomonnaie/src/main/java/com/cda/jee/controller/CurrencyListServlet.java
+++ cryptomonnaie/src/main/java/com/cda/jee/controller/CurrencyListServlet.java
@@ -1,6 +1,7 @@
 package com.cda.jee.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,11 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.cda.jee.model.Currency;
+import com.cda.jee.services.CurrencyServicesImp;
+
 @WebServlet("/currency_index.html")
 public class CurrencyListServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		CurrencyServicesImp currencyServices = new CurrencyServicesImp();
+		
+		ArrayList<Currency> currencies = currencyServices.index();
+		
+		req.setAttribute("currencies", currencies);
 		
 		req.getRequestDispatcher("/WEB-INF/currency_list.jsp").forward(req, resp);
 	}
--- cryptomonnaie/src/main/webapp/WEB-INF/currency_list.jsp
+++ cryptomonnaie/src/main/webapp/WEB-INF/currency_list.jsp
@@ -1,2 +1,24 @@
+<div class="container">
+	<h1>Liste des cryptomonnaies</h1>
 
-<h1>Liste</h1>
+	<table class="table table-striped">
+		<thead class="thead-dark">
+			<tr>
+				<th scope="col">ID</th>
+				<th scope="col">Name</th>
+				<th scope="col">Label</th>
+				<th scope="col">Prix Actuel</th>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach items="${currencies}" var="currency">
+				<tr>
+					<td><c:out value="${currency.getIdCurrency() }"></c:out></td>
+					<td><c:out value="${currency.getNameCurrency() }"></c:out></td>
+					<td><c:out value="${currency.getLabel() }"></c:out></td>
+					<td><c:out value="${currency.getCurrentPrice() }"></c:out></td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
+</div>
