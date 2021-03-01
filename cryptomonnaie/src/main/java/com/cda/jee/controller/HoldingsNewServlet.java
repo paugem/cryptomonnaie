d56ffe7b8b240fdc703e20cc305ade5df795f946
Antoine FRANCOIS <francois.antoine3@gmail.com>
2021-03-01 09:29:44 +0100

    add the types to the inputs

--- cryptomonnaie/src/main/java/com/cda/jee/controller/HoldingsNewServlet.java
+++ cryptomonnaie/src/main/java/com/cda/jee/controller/HoldingsNewServlet.java
@@ -33,6 +33,7 @@ public class HoldingsNewServlet extends HttpServlet {
 		String quantityStr = req.getParameter("quantity");
 		String purchasePriceStr = req.getParameter("purchasePrice");
 		String purchaseDateStr = req.getParameter("purchaseDate");
+		System.out.println(purchaseDateStr);
 
 		// Check if every parameters has been completed
 		if (nameCurrency == null || nameCurrency.isEmpty() || quantityStr == null || quantityStr.isEmpty()
